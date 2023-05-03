9d5419ae322c80a1ab3e6830ea3b57f5f4283af5
mehmetyasincirakpy <myasincirak@gmail.com>
2023-05-03 18:25:58 +0300

    .
    Signed-off-by: mehmetyasincirakpy <myasincirak@gmail.com>

--- Diyabet_Feature_Engineering.py
+++ Diyabet_Feature_Engineering.py
@@ -1,23 +1,20 @@
-import pandas
-import numpy
-import seaborn
-import matplotlib.pyplot as plot
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,roc_auc_score
 import warnings
+
+import matplotlib.pyplot as plt
+import pandas
+import seaborn
+
 warnings.simplefilter(action="ignore")
 
-pandas.set_option("display.max_columns",None)
-pandas.set_option("display.width",170)
-pandas.set_option("display.max_rows",20)
-pandas.set_option("display.float_format",lambda x: "%.3f" %x)
+pandas.set_option("display.max_columns", None)
+pandas.set_option("display.width", 170)
+pandas.set_option("display.max_rows", 20)
+pandas.set_option("display.float_format", lambda x: "%.3f" % x)
 
-dataframe=pandas.read_csv("Datasets/diabetes.csv")
+dataframe = pandas.read_csv("Datasets/diabetes.csv")
 
-def check_dataframe(dataframee,head=5):
+
+def check_dataframe(dataframee, head=5):
     print("############################# Shape ############################# ")
     print(dataframee.shape)
     print("############################# Dtypes ############################# ")
@@ -29,65 +26,55 @@ def check_dataframe(dataframee,head=5):
     print("############################# NA ############################# ")
     print(dataframee.isnull().sum())
     print("############################# Quantiles ############################# ")
-    print(dataframee.quantile([0,0.05,0.50,0.95,0.99,1]).T)
+    print(dataframee.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
 
 check_dataframe(dataframe)
 
+
 def grab_col_names(dataframee, cat_th=10, car_th=20):
-    """
-
-    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
-    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
-
-    Parameters
-    ------
-        dataframe: dataframe
-                Değişken isimleri alınmak istenilen dataframe
-        cat_th: int, optional
-                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
-        car_th: int, optional
-                kategorik fakat kardinal değişkenler için sınıf eşik değeri
-
-    Returns
-    ------
-        cat_cols: list
-                Kategorik değişken listesi
-        num_cols: list
-                Numerik değişken listesi
-        cat_but_car: list
-                Kategorik görünümlü kardinal değişken listesi
-
-    Examples
-    ------
-        import seaborn as sns
-        df = sns.load_dataset("iris")
-        print(grab_col_names(df))
-
-
-    Notes
-    ------
-        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
-        num_but_cat cat_cols'un içerisinde.
-
-    """
-    # cat_cols, cat_but_car
-    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
-    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
-    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    cat_cols = [col for col in dataframee.columns if dataframee[col].dtypes == "0"]
+    num_but_cat = [col for col in dataframee.columns if dataframee[col].nunique() < cat_th and dataframee[col].dtypes != "0"]
+    cat_but_car = [col for col in dataframee.columns if dataframee[col].nunique() > car_th and dataframee[col].dtypes == "0"]
     cat_cols = cat_cols + num_but_cat
     cat_cols = [col for col in cat_cols if col not in cat_but_car]
 
-    # num_cols
-    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in dataframee.columns if dataframee[col].dtypes != "0"]
     num_cols = [col for col in num_cols if col not in num_but_cat]
 
-    print(f"Observations: {dataframee.shape[0]}")
+    print(f"Obvervations:{dataframee.shape[0]}")
     print(f"Variables: {dataframee.shape[1]}")
-    print(f'cat_cols: {len(cat_cols)}')
-    print(f'num_cols: {len(num_cols)}')
-    print(f'cat_but_car: {len(cat_but_car)}')
-    print(f'num_but_cat: {len(num_but_cat)}')
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_car: {len(cat_but_car)}")
+    print(f"num_but_cat: {len(num_but_cat)}")
 
     return cat_cols, num_cols, cat_but_car
 
-cat_cols,num_cols,cat_but_car=grab_col_names(dataframe)
+
+cat_cols, num_cols, cat_but_car = grab_col_names(dataframe)
+
+
+def cat_summary(dataframee, col_name, plot=False):
+    print(pandas.DataFrame({col_name: dataframee[col_name].value_counts(), "Ratio": 100 * dataframee[col_name].value_counts() / len(dataframee)}))
+
+    print("#######################################################################")
+    if plot:
+        seaborn.countplot(x=dataframee[col_name], data=dataframee)
+        plt.show()
+
+
+cat_summary(dataframe, "Outcome")
+
+
+def num_summary(dataframee, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframee[numerical_col].describe(quantiles).T)
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+for col in num_cols:
+    num_summary(dataframe,col,plot=True)
